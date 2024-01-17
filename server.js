eea4722dcab1861c89f190fb714e70a7631fa7ad
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2024-01-17 20:37:31 +0200

    test

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-import app from "./app.js"
-// cQHyeQ6WNQDJrKPU
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+import mongoose from "mongoose";
+import app from "./app.js";
 
-// xkKBIwruXEsbxM1a
+const DB_HOST =
+  "mongodb+srv://teoss902:xkKBIwruXEsbxM1a@cluster0.q6r4guo.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// cQHyeQ6WNQDJrKPU
