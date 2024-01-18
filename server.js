325889ed0143a4a3791a1afc1a35174f4c69f2d8
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2024-01-18 16:03:39 +0200

    com DB_host & port

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const DB_HOST =
-  "mongodb+srv://teoss902:xkKBIwruXEsbxM1a@cluster0.q6r4guo.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
+const { DB_HOST, PORT = 3000 } = process.env;
+console.log(process.env)
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Database connection successful. Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// cQHyeQ6WNQDJrKPU
